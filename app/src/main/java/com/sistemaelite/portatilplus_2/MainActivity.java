9276e8618be3d3659c1a3194c5ecf49c302791bd
FREDDYGF <fregiraldo1@misena.edu.co>
2024-07-01 13:07:14 -0500

    mi primer commit

---
+++ app/src/main/java/com/sistemaelite/portatilplus_2/MainActivity.java
@@ -0,0 +1,106 @@
+package com.sistemaelite.portatilplus_2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.sistemaelite.portatilplus_2.network.MySingleton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText editTextEmail, editTextPassword;
+    private Button buttonLogin, btnRegistro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        editTextEmail = findViewById(R.id.textInputLayout2);
+        editTextPassword = findViewById(R.id.textInputLayout);
+        buttonLogin = findViewById(R.id.Iniciar_sesion);
+        btnRegistro = findViewById(R.id.btn_registro);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistroActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void loginUser() {
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        String url = "https://apiportatilplus.onrender.com/login";
+
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("email", email);
+            postData.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, postData,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            boolean success = response.getBoolean("success");
+                            if (success) {
+                                Toast.makeText(MainActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
+                                // Proceed to next activity or save session token
+                            } else {
+                                Toast.makeText(MainActivity.this, "Login failed!", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Login Error", error.toString());
+                Toast.makeText(MainActivity.this, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+    }
+}
---
+++ app/src/main/java/com/sistemaelite/portatilplus_2/RegistroActivity.java
@@ -0,0 +1,125 @@
+package com.sistemaelite.portatilplus_2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.sistemaelite.portatilplus_2.network.MySingleton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class RegistroActivity extends AppCompatActivity {
+
+    private EditText editTextNombres, editTextApellidos, editTextEmail, editTextTelefono, editTextPassword, editTextConfirmPassword;
+    private Button btnInicio, btnCrear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_registro);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        editTextNombres = findViewById(R.id.textInputLayout3);
+        editTextApellidos = findViewById(R.id.textInputLayout4);
+        editTextEmail = findViewById(R.id.textInputLayout5);
+        editTextTelefono = findViewById(R.id.textInputLayout6);
+        editTextPassword = findViewById(R.id.textInputLayout7);
+        editTextConfirmPassword = findViewById(R.id.textInputLayout8);
+        btnInicio = findViewById(R.id.btn_inicio);
+        btnCrear = findViewById(R.id.btn_crear);
+
+        btnInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegistroActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnCrear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
+    }
+
+    private void registerUser() {
+        String nombres = editTextNombres.getText().toString();
+        String apellidos = editTextApellidos.getText().toString();
+        String email = editTextEmail.getText().toString();
+        String telefono = editTextTelefono.getText().toString();
+        String password = editTextPassword.getText().toString();
+        String confirmPassword = editTextConfirmPassword.getText().toString();
+
+        if (!password.equals(confirmPassword)) {
+            Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String url = "https://apiportatilplus.onrender.com/register";
+
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("nombre", nombres);
+            postData.put("apellido", apellidos);
+            postData.put("correo", email);
+            postData.put("telefono", telefono);
+            postData.put("contrasena", password);
+            postData.put("rol", "usuario");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, postData,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            boolean success = response.getBoolean("success");
+                            if (success) {
+                                Toast.makeText(RegistroActivity.this, "Registration successful!", Toast.LENGTH_SHORT).show();
+                                // Redirect to login or main activity
+                                Intent intent = new Intent(RegistroActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                Toast.makeText(RegistroActivity.this, "Registration failed!", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Registration Error", error.toString());
+                Toast.makeText(RegistroActivity.this, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+    }
+}
