4136e857a71d4548e047d37cac6499301109b021
JnSsanRico <juan0096150@gmail.com>
2024-07-01 22:55:44 -0500

    ya funciona el slider

--- app/src/main/java/com/sistemaelite/portatilplus_2/Fragment/MainFragment.java
+++ app/src/main/java/com/sistemaelite/portatilplus_2/Fragment/MainFragment.java
@@ -8,14 +8,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.sistemaelite.portatilplus_2.R;
+
+import java.util.ArrayList;
 
 public class MainFragment extends Fragment {
 
+    private ImageSlider imageSlider;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        imageSlider = view.findViewById(R.id.image_slider);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        setupImageSlider();
+        return view;
+    }
+    private void setupImageSlider() {
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
+        slideModels.add(new SlideModel(R.drawable.img_1, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.img_2, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.img_3, ScaleTypes.FIT));
+        imageSlider.setImageList(slideModels, ScaleTypes.FIT);
     }
 }
