b95cd1330e9102abc3900b9ec4d63c33fa990008
FREDDYGF <fregiraldo1@misena.edu.co>
2024-07-05 22:16:08 -0500

    funciona barra de busqueda

--- app/src/main/java/com/sistemaelite/portatilplus_2/HomeActivity.java
+++ app/src/main/java/com/sistemaelite/portatilplus_2/HomeActivity.java
@@ -3,6 +3,8 @@ package com.sistemaelite.portatilplus_2;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -10,11 +12,9 @@ import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.graphics.Insets;
 import androidx.core.view.GravityCompat;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
@@ -25,7 +25,6 @@ import com.sistemaelite.portatilplus_2.Fragment.SettingsFragment;
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawerLayout;
-    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,15 +32,14 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
 
-        Toolbar toolbar = findViewById(R.id.toolbar); //Ignore red line errors
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         drawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
-                R.string.close_nav);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
@@ -50,42 +48,61 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
             navigationView.setCheckedItem(R.id.nav_home);
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Maneja la búsqueda cuando se envía el texto
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Maneja la búsqueda mientras el texto cambia
+                return false;
+            }
+        });
+
+        return true;
+    }
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
 
-        if (item.getItemId() == R.id.nav_home) {
-            // Código para nav_home
+        if (id == R.id.nav_home) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MainFragment()).commit();
-        } else if (item.getItemId() == R.id.nav_settings) {
-            // Código para nav_settings
+        } else if (id == R.id.nav_settings) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
-        } else if (item.getItemId() == R.id.nav_bookmark) {
-            // Código para nav_share
+        } else if (id == R.id.nav_bookmark) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ReservaFragment()).commit();
-        } else if (item.getItemId() == R.id.nav_about) {
+        } else if (id == R.id.nav_about) {
             // Código para nav_about
-        } else if (item.getItemId() == R.id.nav_logout) {
-            // Código para cerrar sesion
-            // Obtener el SharedPreferences
+        } else if (id == R.id.nav_logout) {
             SharedPreferences preferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
             SharedPreferences.Editor editor = preferences.edit();
-
-            // Limpiar los datos de sesión
             editor.clear();
             editor.apply();
 
-            // Mostrar mensaje de cierre de sesión
             Toast.makeText(HomeActivity.this, "¡Sesión cerrada exitosamente!", Toast.LENGTH_SHORT).show();
-
-            // Redirigir al usuario a la pantalla de inicio de sesión
             Intent intent = new Intent(HomeActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
         }
+
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
 
+    @Override
     public void onBackPressed() {
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
